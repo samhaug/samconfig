c1bb02430b386ee1fd4411c123b65173964b9289
samhaug <haugland21@gmail.com>
2021-01-13 15:11:39 -0700

    mkdir

--- setup.sh
+++ setup.sh
@@ -8,38 +8,42 @@ echo "      CONFIGURATION SETUP             "
 echo "######################################"
 echo ""
 
-echo "Linking vimrc and template files"
-ln -vfrs ./vim/vimrc $HOME/.vimrc
-for i in $(ls ./vim/skeleton* | awk -F"/" '{print $NF}') ; do 
-   ln -vfrs ./vim/$i $HOME/.vim/template/$i
-done
-
-if [ $? != 0 ]; then
-   echo "WARNING: something wrong with vimrc"
-fi
-
-echo ""
-echo "######################################"
-echo ""
-
-echo "Linking bashrc"
-ln -vfrs ./bash/bashrc $HOME/.bashrc
-source $HOME/.bashrc
-
-if [ $? != 0 ]; then
-   echo "WARNING: something wrong with bashrc"
-fi
+   echo "Linking vimrc and template files"
+   if [ ! -d ./vim/template ]; then
+      echo "making ./vim/template"
+      mkdir ./vim/template
+   fi
+   ln -vfrs ./vim/vimrc $HOME/.vimrc
+   for i in $(ls ./vim/skeleton* | awk -F"/" '{print $NF}') ; do 
+      ln -vfrs ./vim/$i $HOME/.vim/template/$i
+   done
+   
+   if [ $? != 0 ]; then
+      echo "WARNING: something wrong with vimrc"
+   fi
 
 echo ""
 echo "######################################"
 echo ""
 
-echo "Linking ipython startup"
-ln -vfrs ipython/startup.py $HOME/.ipython/profile_default/startup/startup.py
+   echo "Linking bashrc"
+   ln -vfrs ./bash/bashrc $HOME/.bashrc
+   source $HOME/.bashrc
+   
+   if [ $? != 0 ]; then
+      echo "WARNING: something wrong with bashrc"
+   fi
 
-if [ $? != 0 ]; then
-   echo "WARNING: something wrong with ipython default"
-fi
+echo ""
+echo "######################################"
+echo ""
+
+   echo "Linking ipython startup"
+   ln -vfrs ipython/startup.py $HOME/.ipython/profile_default/startup/startup.py
+   
+   if [ $? != 0 ]; then
+      echo "WARNING: something wrong with ipython default"
+   fi
 
 echo ""
 echo "######################################"
