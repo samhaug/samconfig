bfa580b2f603b392e859c7e237ae45a9ea58150c
Haugland <samhaug@mbp-samhaug.local>
2021-03-18 21:40:52 -0400

    fixed setup.sh

--- setup.sh
+++ setup.sh
@@ -2,21 +2,31 @@
 
 #Run this routine to link all configuration files to their proper locations
 
-echo ""
+cwd=$(pwd)
+
+echo " "
 echo "######################################"
 echo "      CONFIGURATION SETUP             "
 echo "######################################"
-echo ""
+echo " "
+
+   echo "Linking vimrc"
+
+   if [ -f -a $HOME/.vimrc ]; then
+      echo "removing $HOME/.vimrc"
+      rm $HOME/.vimrc
+   fi
+
+   ln -vfs $cwd/vim/vimrc $HOME/.vimrc
+
 
    echo "Linking vimrc and template files"
-   if [ ! -d ./vim/template ]; then
-      echo "making ./vim/template"
-      mkdir ./vim/template
-   fi
-   ln -vfs ./vim/vimrc $HOME/.vimrc
-   for i in $(ls ./vim/skeleton* | awk -F"/" '{print $NF}') ; do 
-      ln -vfs ./vim/$i $HOME/.vim/template/$i
-   done
+    #if [ -d -a  $HOME/.vim ]; then
+    #      echo "removing $HOME/.vim"
+    #      rm -rf $HOME/.vim
+   #   fi
+
+   ln -Fs $cwd/vim/ $HOME/.vim
    
    if [ $? != 0 ]; then
       echo "WARNING: something wrong with vimrc"
@@ -27,7 +37,7 @@ echo "######################################"
 echo ""
 
    echo "Linking bashrc"
-   ln -vfs ./bash/bashrc $HOME/.bashrc
+   ln -vfs $cwd/bash/bashrc $HOME/.bashrc
    source $HOME/.bashrc
    
    if [ $? != 0 ]; then
@@ -39,7 +49,7 @@ echo "######################################"
 echo ""
 
    echo "Linking ipython startup"
-   ln -vfs ipython/startup.py $HOME/.ipython/profile_default/startup/startup.py
+   ln -vfs $cwd/ipython/startup.py $HOME/.ipython/profile_default/startup/startup.py
    
    if [ $? != 0 ]; then
       echo "WARNING: something wrong with ipython default"
@@ -49,4 +59,7 @@ echo ""
 echo "######################################"
 echo ""
 
+if [ $? == 0 ]; then
+   echo "SUCCESS! All is well"
+fi
 
